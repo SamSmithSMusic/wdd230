925a461ac8b1929a219c69e6125ba2d141300967
Samuel Smith <samsmithsmusic@gmail.com>
2024-09-21 02:10:37 -0600

    Home Page Done

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,18 @@
-let name = document.querySelector('#name');
+let date = new Date();
+let year = date.getFullYear();
+
+let nameObj = document.createElement('p');
+let modified = document.createElement('p');
+
+nameObj.setAttribute('id','name');
+modified.setAttribute('id','modifi');
+
+const parent = document.querySelector('footer');
+
+let moddedDate = document.lastModified;
+
+nameObj.innerHTML = `&copy${year} | Samuel Smith | America 🇺🇸`;
+modified.innerHTML = `Last Modified : ${moddedDate}`;
+
+parent.appendChild(nameObj);
+parent.appendChild(modified);
