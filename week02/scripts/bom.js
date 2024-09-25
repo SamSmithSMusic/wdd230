438dd13820bb9ad2842fca1f75adb320940718ed
SamSmithSMusic <122060875+SamSmithSMusic@users.noreply.github.com>
2024-09-24 20:54:11 -0600

    middler of menu activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#input');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+
+    
+
+});
