4cca75d83e8cdd9e308cb7ced431c22543364138
SamSmithSMusic <122060875+SamSmithSMusic@users.noreply.github.com>
2024-09-25 17:42:27 -0600

    BOM and home fix

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,29 @@
-const input = document.querySelector('#input');
+const input = document.querySelector('input');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
 
-    
+    if (input.value !='') {
+        const li = document.createElement('li');
+        const delButton = document.createElement('button');
 
+        li.innerHTML = input.value;
+        delButton.textContent =  '❌';
+
+        li.append(delButton);
+        list.append(li);
+
+        delButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+            input.value = '';
+        })
+
+    }
+    else {
+        input.setAttribute('placeholder','Please Enter Your Book');
+        input.focus();
+    }
 });
+
