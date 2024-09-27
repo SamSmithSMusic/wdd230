d1f0e323e2aa5c3864dc64b230b3b7febd9d150c
Samuel Smith <samsmithsmusic@gmail.com>
2024-09-27 14:48:36 -0600

    darkButton needs card text fix

--- scripts/buttonResponse.js
+++ scripts/buttonResponse.js
@@ -5,3 +5,20 @@ button.addEventListener('click', () => {
     navlist.classList.toggle('seen');
     button.classList.toggle('seen');
 });
+
+const darkButt = document.querySelector('#darkmode');
+const background = document.querySelector('body');
+const darktext =  document.querySelector('h1');
+const listText = document.querySelector('.card ul');
+const links = document.querySelectorAll('.card a');
+
+darkButt.addEventListener('click', () => {
+    background.classList.toggle('darkBackground');
+    darktext.classList.toggle('whiteText');
+    darkButt.classList.toggle('dark');
+    listText.classList.toggle('whiteText');
+    links.forEach((link) => {
+        link.classList.toggle('whiteText');
+    })
+
+})
