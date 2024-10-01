9baaa4e93c89b1a666f253adf76f463281338dbe
Samuel Smith <samsmithsmusic@gmail.com>
2024-09-30 22:28:59 -0600

    chamber home done, needs member font size

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -5,3 +5,8 @@ menuButt.addEventListener('click', () => {
     menuButt.classList.toggle('expanded');
     nav.classList.toggle('expanded');
 })
+
+let date = new Date();
+const year = date.getFullYear();
+const footer = document.querySelector('footer');
+footer.innerHTML = `Samuel Smith &copy ${year}<br>WDD 230<br>Last Modified - ${document.lastModified}`;
