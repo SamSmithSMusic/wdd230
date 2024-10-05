df1db55b1af6371e4af43ad752664b03793dcca3
Samuel Smith <samsmithsmusic@gmail.com>
2024-10-05 00:01:53 -0600

    Discover Finished

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -10,3 +10,35 @@ let date = new Date();
 const year = date.getFullYear();
 const footer = document.querySelector('footer');
 footer.innerHTML = `Samuel Smith &copy ${year}<br>WDD 230<br>Last Modified - ${document.lastModified}`;
+
+
+const counter = document.querySelector('#cards p');
+let storedText = localStorage.getItem('lastVisit') || 0;
+const storedDate = new Date(storedText);
+const currentDate = new Date();
+
+let daysSince = dateDiff(currentDate, storedDate);
+let message = '';
+if (daysSince == 0) {
+    message = 'Welcome! Let us know if you have any questions.';
+}
+else if (daysSince <1) {
+    message = 'Back so soon? Awesome!'
+}
+else {
+    if (daysSince == 1) {
+        message = `You last visited 1 day ago.`
+    }
+    else {
+        message = `You last visited ${daysSince} days ago.`
+    }
+    
+}
+counter.innerHTML = message
+localStorage.setItem('lastVisit', currentDate);
+
+function dateDiff(date1, date2) {
+    const timeBetween = Math.abs(date1 - date2);
+    const daysBetween = Math.floor(timeBetween / 86400000);
+    return daysBetween
+}
