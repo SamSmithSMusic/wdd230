e00f340294daa2d5cdcdc155314274c7e6a25ddb
SamSmithSMusic <122060875+SamSmithSMusic@users.noreply.github.com>
2024-10-10 15:02:05 -0600

    edits

--- scripts/formstuff.js
+++ scripts/formstuff.js
@@ -13,8 +13,14 @@ const pass1 = document.querySelector('#pass1');
 const pass2 = document.querySelector('#pass2');
 const message = document.querySelector('#passworderror');
 
-pass2.addEventListener('focusout',checkPass)
+pass2.addEventListener('focusout',checkpass)
 
 function checkpass() {
+    message.classList.add('hidden');
+    if (pass1.value != pass2.value) {
+        message.classList.remove('hidden');
+        pass2.value = '';
+        pass2.focus();
+    }
 
 }
