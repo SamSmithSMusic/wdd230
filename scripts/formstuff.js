1cf7516eeed02524da08674040bde824bcaa9685
SamSmithSMusic <122060875+SamSmithSMusic@users.noreply.github.com>
2024-10-10 11:04:23 -0600

    js form

--- scripts/formstuff.js
+++ scripts/formstuff.js
@@ -7,3 +7,14 @@ range.addEventListener('input', displayRating);
 function displayRating() {
     rangevalue.innerHTML = range.value;
 }
+
+
+const pass1 = document.querySelector('#pass1');
+const pass2 = document.querySelector('#pass2');
+const message = document.querySelector('#passworderror');
+
+pass2.addEventListener('focusout',checkPass)
+
+function checkpass() {
+
+}
