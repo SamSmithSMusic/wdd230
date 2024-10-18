e0c5a9fd171f34319631298d2f2aa639034e7558
SamSmithSMusic <122060875+SamSmithSMusic@users.noreply.github.com>
2024-10-17 20:16:06 -0600

    weather done

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -24,3 +24,34 @@ count ++;
 localStorage.setItem('pageCount', count);
 
 counter.innerHTML = `Page Visit Number : ${count}`;
+
+
+// Weather functionality
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=29.70&lon=98.12&appid=82175ebfabf6834db5939dad1beb443f&units=imperial';
+
+async function weatherFetch() {
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const weather = document.querySelector('#weather');
+
+function displayResults(data) {
+    const iconsrc = ` https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weather.innerHTML = `<img src="${iconsrc}" alt="Weather Icon"> ${data.main.temp}&deg;F ${data.weather[0].description}`;
+}
+
+weatherFetch();
